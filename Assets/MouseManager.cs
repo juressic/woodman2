b20c734e0cb812d370cc89be9479d10b3f2b206e
Jure <juremiskov@gmail.com>
2019-01-26 10:57:54 +0100

    woodman2 test
    test

---
+++ Assets/MouseManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseManager : MonoBehaviour
+{
+
+    public LayerMask clickable;
+
+    public EventVector3 OnClickEnviroment;
+
+    public EventVector3 OnClickTree;
+
+    public Texture2D terrain;
+    public Texture2D offTerrain;
+    public Texture2D doorway;
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity,clickable.value))
+        {
+            bool door = false;
+            if (hit.collider.gameObject.tag == "Terrain")
+            {
+                Cursor.SetCursor(terrain, new Vector2(16, 16), CursorMode.Auto);
+            }
+            else if(hit.collider.gameObject.tag == "doorway")
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(door == true)
+                {
+                    Transform doorway1 = hit.collider.gameObject.transform;
+                    OnClickEnviroment.Invoke(doorway1.position + doorway1.forward * 10);
+                    //Debug.Log("doorway");
+                }
+                OnClickEnviroment.Invoke(hit.point);
+            }
+        }
+        else
+        {
+            Cursor.SetCursor(offTerrain, new Vector2(16, 16), CursorMode.Auto);
+        }
+    }
+}
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
---
+++ Assets/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour {
+
+
+    public TerrainCollider map;
+
+    public NavMeshAgent agent;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private Vector3? ScreenMapLook()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (!map.Raycast(ray, out hit, Mathf.Infinity))
+            return null;
+
+        return hit.point;
+    }
+    
+	void Update () {
+
+        Debug.Log(transform.localRotation.y);
+        anim.SetFloat("Move", agent.velocity.magnitude);
+        //anim.SetFloat("Side", transform.localRotation.y);
+        //anim.SetFloat("YRotation", transform.localRotation.y);
+        //GetComponent<Animator>().SetFloat("Move", transform.localRotation.y);
+
+
+        if (Input.GetMouseButton(1))
+        {
+            transform.LookAt(ScreenMapLook().Value);
+            anim.SetBool("RightClick", true);
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            transform.rotation = transform.parent.rotation;
+            anim.SetBool("RightClick", false);
+        }
+	}
+}
